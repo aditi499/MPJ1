531c625a151be6fc76e410830e963d6cb1ca735f
aditi499 <aditisingh.hbl@gmail.com>
2026-02-24 23:20:53 +0530

    Add files via upload

---
+++ Assignment_2.java
@@ -0,0 +1,54 @@
+class Employee {
+    double salary;
+
+    Employee(double salary) {
+        this.salary = salary;
+    }
+
+    void displaySalary() {
+        System.out.println("Salary: " + salary);
+    }
+}
+
+class FullTimeEmployee extends Employee {
+
+    FullTimeEmployee(double salary) {
+        super(salary);
+    }
+
+    void calculateSalary() {
+        salary = salary + (salary * 0.50);
+    }
+}
+
+class InternEmployee extends Employee {
+
+    InternEmployee(double salary) {
+        super(salary);
+    }
+    void calculateSalary() {
+        salary = salary + (salary * 0.25);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        FullTimeEmployee ft = new FullTimeEmployee(50000);
+        InternEmployee intern = new InternEmployee(20000);
+
+        System.out.println("Full Time Employee:");
+        System.out.println("Before Hike:");
+        ft.displaySalary();
+        ft.calculateSalary();
+        System.out.println("After 50% Hike:");
+        ft.displaySalary();
+
+        System.out.println("\nIntern Employee:");
+        System.out.println("Before Hike:");
+        intern.displaySalary();
+        intern.calculateSalary();
+        System.out.println("After 25% Hike:");
+        intern.displaySalary();
+    }
+}
